07b722719d2f5cb9285fa09d3eb1c8d61d12fb05
HeartArmy <68476161+HeartArmy@users.noreply.github.com>
2025-11-16 19:05:31 +0400

    added more general csp

--- next.config.js
+++ next.config.js
@@ -19,7 +19,7 @@ const nextConfig = {
         headers: [
           {
             key: 'Content-Security-Policy',
-            value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.youtube.com https://www.youtube-nocookie.com; style-src 'self' 'unsafe-inline' https://www.youtube.com https://www.youtube-nocookie.com; img-src 'self' data: https:; font-src 'self' data:; connect-src 'self' https://www.youtube.com https://www.youtube-nocookie.com wss://ws-ap2.pusher.com https://sockjs-ap2.pusher.com https://uploadthing-prod-sea1.s3.us-west-2.amazonaws.com; frame-src 'self' https://www.youtube-nocookie.com; frame-ancestors 'none'; object-src 'none'; base-uri 'self'; form-action 'self';",
+            value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.youtube.com https://www.youtube-nocookie.com; style-src 'self' 'unsafe-inline' https://www.youtube.com https://www.youtube-nocookie.com; img-src 'self' data: https: https://*.utfs.io; font-src 'self' data:; connect-src 'self' https://www.youtube.com https://www.youtube-nocookie.com wss://ws-ap2.pusher.com https://sockjs-ap2.pusher.com https://uploadthing-prod-sea1.s3.us-west-2.amazonaws.com https://api.uploadthing.com https://*.uploadthing.com https://*.utfs.io; frame-src 'self' https://www.youtube-nocookie.com; frame-ancestors 'none'; object-src 'none'; base-uri 'self'; form-action 'self';",
           },
         ],
       },
