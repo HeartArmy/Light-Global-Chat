39d05b1755c66bda8cff990fa7d58c264722eae3
HeartArmy <68476161+HeartArmy@users.noreply.github.com>
2025-11-16 19:38:21 +0400

    added  geoloocation csp

--- next.config.js
+++ next.config.js
@@ -21,6 +21,10 @@ const nextConfig = {
             key: 'Content-Security-Policy',
             value: "default-src * 'unsafe-inline' 'unsafe-eval'; script-src * 'unsafe-inline' 'unsafe-eval'; connect-src * 'unsafe-inline'; img-src * data: blob: 'unsafe-inline'; frame-src *; style-src * 'unsafe-inline';",
           },
+          {
+            key: 'Permissions-Policy',
+            value: 'geolocation=(self), microphone=(), camera=(), fullscreen=(self)',
+          },
         ],
       },
     ];
