634fb0d4d5e205da42f560b93a9caeaf10cb3330
HeartArmy <68476161+HeartArmy@users.noreply.github.com>
2025-11-16 18:31:05 +0400

    added yotuube to csp

--- next.config.js
+++ next.config.js
@@ -12,6 +12,19 @@ const nextConfig = {
   experimental: {
     missingSuspenseWithCSRBailout: false,
   },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.youtube.com https://www.youtube-nocookie.com; style-src 'self' 'unsafe-inline' https://www.youtube.com https://www.youtube-nocookie.com; img-src 'self' data: https:; font-src 'self' data:; connect-src 'self' https://www.youtube.com https://www.youtube-nocookie.com; frame-src 'self' https://www.youtube-nocookie.com; frame-ancestors 'none'; object-src 'none'; base-uri 'self'; form-action 'self';",
+          },
+        ],
+      },
+    ];
+  },
 }
 
 module.exports = nextConfig
