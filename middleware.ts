33732c94b0270e8b2baa62e7c09055c8b4d58a51
HeartArmy <68476161+HeartArmy@users.noreply.github.com>
2025-11-30 14:57:55 +0400

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,5 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
-import '@/lib/logger'; // Enable MongoDB logging interception
 
 export function middleware(request: NextRequest) {
   const response = NextResponse.next();
