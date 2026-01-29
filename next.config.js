f959a1d7eee490ce3dfffa8259e09987da2ff2a3
HeartArmy <68476161+HeartArmy@users.noreply.github.com>
2026-01-29 14:01:36 +0400

    added vercel analytics

--- next.config.js
+++ next.config.js
@@ -19,7 +19,7 @@ const nextConfig = {
         headers: [
           {
             key: 'Content-Security-Policy',
-            value: "default-src 'self' * 'unsafe-inline' 'unsafe-eval'; script-src 'self' * 'unsafe-inline' 'unsafe-eval'; connect-src 'self' * wss: ws:; img-src 'self' * data: blob: 'unsafe-inline'; frame-src 'self' *; style-src 'self' * 'unsafe-inline';",
+            value: "default-src 'self' * 'unsafe-inline' 'unsafe-eval'; script-src 'self' * 'unsafe-inline' 'unsafe-eval' https://va.vercel-scripts.com; connect-src 'self' * wss: ws: https://va.vercel-scripts.com; img-src 'self' * data: blob: 'unsafe-inline'; frame-src 'self' *; style-src 'self' * 'unsafe-inline';",
           },
           {
             key: 'Permissions-Policy',
